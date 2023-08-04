c0fad18659766f5f912a0c97971ca01a4ebf4a10
bfar co <bfarco@bfarco.local>
2023-08-04 15:52:37 +0800

    first commit

---
+++ src/components/work/Data.jsx
@@ -0,0 +1,54 @@
+import Work1 from "../../assets/work1.jpg";
+import Work2 from "../../assets/work2.jpg";
+import Work3 from "../../assets/work3.jpg";
+import Work4 from "../../assets/work4.jpg";
+import Work5 from "../../assets/work5.jpg";
+
+
+export const projectsData = [
+    {
+        id: 1,
+        image: Work1,
+        title: 'Web Design',
+        category: 'Design',
+    },
+    {
+        id: 2,
+        image: Work2,
+        title: 'Mobile Application',
+        category: 'App',
+    },
+    {
+        id: 3,
+        image: Work3,
+        title: 'Brand Design',
+        category: 'Design',
+    },
+    {
+        id: 4,
+        image: Work4,
+        title: 'Develop Webpage',
+        category: 'Web',
+    },
+    {
+        id: 5,
+        image: Work5,
+        title: 'Database Setup',
+        category: 'App',
+    },
+];
+
+export const projectsNav = [
+    {
+        name: 'all',
+    },
+    {
+        name: 'Web',
+    },
+    {
+        name: 'App',
+    },
+    {
+        name: 'Design',
+    },
+];
